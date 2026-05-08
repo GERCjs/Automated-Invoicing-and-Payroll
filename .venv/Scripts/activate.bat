ad4928df481f3002441cee9b34d25646be6c162a
Hespie210 <hespiecya@gmail.com>
2026-05-08 16:39:28 +0800

    .venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\RP\YR3S1\FYP\Automated Invoicing and Payroll\Automated Invoicing and Payroll\.venv"
+set "VIRTUAL_ENV=C:\Automated-Invoicing-and-Payroll\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
